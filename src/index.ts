20db9979bae92d84328377af7517b1bbe6b98184
mrtrvl <mrt@mrt.ee>
2022-09-22 20:31:31 +0300

    Postituste ja postituste staatustega seotud ressursid

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import express, { Request, Response } from 'express';
-import { createModuleResolutionCache } from 'typescript';
 const app = express();
 const PORT = 3000;
 
@@ -16,6 +15,25 @@ interface IUser extends INewUser {
     id: number;
 }
 
+interface INewPost {
+    userId: number;
+    title: string;
+    content: string;
+    statusId: number;
+}
+
+interface IPost extends INewPost {
+    id: number;
+}
+
+interface INewPostStatus {
+    status: string;
+}
+
+interface IPostStatus extends INewPostStatus {
+    id: number;
+}
+
 const users: IUser[] = [
     {
         id: 1,
@@ -23,6 +41,38 @@ const users: IUser[] = [
         lastName: 'Juurikas',
         email: 'juhan@juurikas.ee',
         password: 'juhan',
+    },
+];
+
+const posts: IPost[] = [
+    {
+        id: 1,
+        title: 'Esimene postitus',
+        content: 'Esimese postituse sisu',
+        userId: 2,
+        statusId: 7,
+    },
+    {
+        id: 2,
+        title: 'Teine postitus',
+        content: 'Teise postituse sisu',
+        userId: 1,
+        statusId: 2,
+    },
+];
+
+const postStatuses: IPostStatus[] = [
+    {
+        id: 1,
+        status: 'Draft',
+    },
+    {
+        id: 2,
+        status: 'Public',
+    },
+    {
+        id: 3,
+        status: 'Private',
     },
 ];
 
@@ -65,7 +115,6 @@ app.get('/api/v1/users/:id', (req: Request, res: Response) => {
         }
     });
 });
-
 
 // Kasutaja muutmine
 app.patch('/api/v1/users/:id', (req: Request, res: Response) => {
@@ -137,6 +186,167 @@ app.delete('/api/v1/users/:id', (req: Request, res: Response) => {
     return res.status(200).json({
         success: true,
         message: `User deleted`,
+    });
+});
+
+// Postituse loomine
+app.post('/api/v1/posts', (req: Request, res: Response) => {
+    const { title, content, userId, statusId } = req.body;
+    if (!title || !content || !userId || !statusId) {
+        return res.status(400).json({
+            success: false,
+            message: `Some data is missing (title, content, userId, statusId)`,
+        });
+    }
+    const id = posts.length + 1;
+    const newPost: IPost = {
+        id,
+        title,
+        content,
+        userId,
+        statusId,
+    };
+    posts.push(newPost);
+
+    return res.status(201).json({
+        success: true,
+        message: `Post with id ${newPost.id} created`,
+    });
+});
+
+// Postituse muutmine
+app.patch('/api/v1/posts/:id', (req: Request, res: Response) => {
+    const id = parseInt(req.params.id);
+    const { title, content, statusId } = req.body;
+    const post = posts.find(element => {
+        return element.id === id;
+    });
+    if (!post) {
+        return res.status(404).json({
+            success: false,
+            message: `Post not found`,
+        });
+    }
+    if (!title && !content && !statusId) {
+        return res.status(400).json({
+            success: false,
+            message: `Nothing to change`,
+        });
+    }
+
+    if (title) post.title = title;
+    if (content) post.content = content;
+    if (statusId) post.statusId = statusId;
+
+    return res.status(200).json({
+        success: true,
+        message: `Post updated`,
+    });
+});
+
+// Postituse kustutamine
+app.delete('/api/v1/posts/:id', (req: Request, res: Response) => {
+    const id = parseInt(req.params.id);
+    const index = posts.findIndex(element => element.id === id);
+    if (index === -1) {
+        return res.status(404).json({
+            success: false,
+            message: `Post not found`,
+        });
+    }
+    posts.splice(index, 1);
+    return res.status(200).json({
+        success: true,
+        message: `Post deleted`,
+    });
+});
+
+// Kõikide postituste staatuste pärimise endpoint
+app.get('/api/v1/posts/statuses', (req: Request, res: Response) => {
+    res.status(200).json({
+        success: true,
+        message: 'List of post statuses',
+        postStatuses,
+    });
+});
+
+// Postituse staatus pärimine staatuse id alusel
+app.get('/api/v1/posts/statuses/:id', (req: Request, res: Response) => {
+    const id = parseInt(req.params.id);
+    const postStatus = postStatuses.find(element => {
+        return element.id === id;
+    });
+    if (!postStatus) {
+        return res.status(404).json({
+            success: false,
+            message: `Post status not found`,
+        });
+    }
+    return res.status(200).json({
+        success: true,
+        message: `Post status`,
+        data: {
+            postStatus,
+        },
+    });
+});
+
+// Kõikide postituste pärimise endpoint
+app.get('/api/v1/posts', (req: Request, res: Response) => {
+    res.status(200).json({
+        success: true,
+        message: 'List of posts',
+        posts
+    });
+});
+
+// Postituse pärimine id alusel
+app.get('/api/v1/posts/:id', (req: Request, res: Response) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find(element => {
+        return element.id === id;
+    });
+    if (!post) {
+        return res.status(404).json({
+            success: false,
+            message: `Post not found`,
+        });
+    };
+    let postStatus: IPostStatus | undefined = postStatuses.find(element => element.id === post.statusId);
+    if(!postStatus) {
+        postStatus = {
+            id: 0,
+            status: 'Unknown',
+        };
+    };
+    
+    let user: IUser | undefined = users.find(elemenet => elemenet.id === post.userId);
+    if (!user) {
+        user = {
+            id: 0,
+            firstName: 'Jane',
+            lastName: 'Doe',
+            email: 'jane@doe.com',
+            password: 'jane',
+        };
+    };
+    const postWithStatusAndUser = {
+        id: post.id,
+        title: post.title,
+        user: {
+            id: user.id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+        },
+        status: postStatus,
+    };
+    return res.status(200).json({
+        success: true,
+        message: `Post`,
+        data: {
+            post: postWithStatusAndUser,
+        },
     });
 });
 
