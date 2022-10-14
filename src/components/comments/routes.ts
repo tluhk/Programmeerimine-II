5ab4cf170a8dc0c5ae4f80d8e2e5e303633486de
mrttlu <71932696+mrttlu@users.noreply.github.com>
2022-10-14 10:41:23 +0300

    ESLint-i lisamine ja selle leitud probleemide korrastamine

--- src/components/comments/routes.ts
+++ src/components/comments/routes.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import commentsController from './controllers';
+
 const commentsRoutes = express.Router();
 
 commentsRoutes
-    .get('/', commentsController.getAllComments)
-    .get('/:id', commentsController.getCommentById)
-    .post('/', commentsController.createComment)
-    .delete('/:id', commentsController.deleteComment);
+  .get('/', commentsController.getAllComments)
+  .get('/:id', commentsController.getCommentById)
+  .post('/', commentsController.createComment)
+  .delete('/:id', commentsController.deleteComment);
 
 export default commentsRoutes;
--- src/components/general/controllers.ts
+++ src/components/general/controllers.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from 'express';
 
 const generalController = {
-    health: (req: Request, res: Response) => {
-        res.status(200).json({
-            message: 'Hello world!',
-        });
-    }
+  health: (req: Request, res: Response) => {
+    res.status(200).json({
+      message: 'API is healthy.',
+    });
+  },
 };
 
 export default generalController;
