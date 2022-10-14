45ee9022c8005f310f1af5b8fe188cd9368ad65b
mrttlu <71932696+mrttlu@users.noreply.github.com>
2022-10-14 13:00:24 +0300

    console.log-i eemaldamine

--- src/components/auth/services.ts
+++ src/components/auth/services.ts
@@ -8,7 +8,6 @@ const jwtSecret = 'lkjhnsdzfkljbnwriuhs<,.mcnQÖIODOIHJoqirhasjdnvkösjfc';
 const authServices = {
   hash: async (password: string): Promise<string> => {
     const hash = await bcrypt.hash(password, saltRounds);
-    console.log(hash);
     return hash;
   },
   compare: async (password: string, hash: string): Promise<Boolean> => {
