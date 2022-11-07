936db670ecce74ca5c3c722fa78ac99865b56a7a
mrtrvl <mrt@mrt.ee>
2022-11-07 12:02:38 +0200

    Kommentaaride päringutele kommentaari looja nime lisamine

--- src/components/comments/services.ts
+++ src/components/comments/services.ts
@@ -6,15 +6,37 @@ import { FieldPacket, ResultSetHeader, RowDataPacket } from 'mysql2';
 
 const commentsService = {
   getAllComments: async (): Promise<ICommentSQL[]> => {
-    const [comments]: [ICommentSQL[], FieldPacket[]] = await pool.query('SELECT * FROM comments WHERE deletedDate IS NULL;');
+    const [comments]: [ICommentSQL[], FieldPacket[]] = await pool.query(
+      `SELECT
+        C.id, C.content, C.createdDate, U.id AS userId, U.firstName, U.lastName
+        FROM
+          comments C
+        INNER JOIN
+          users U ON C.userId = U.id
+        WHERE C.deletedDate IS NULL;`
+      );
     return comments;
   },
   getCommentById: async (id: number): Promise<ICommentSQL> => {
-    const [comments]: [ICommentSQL[], FieldPacket[]] = await pool.query('SELECT * FROM comments WHERE id = ? AND deletedDate IS NULL;', [id]);
+    const [comments]: [ICommentSQL[], FieldPacket[]] = await pool.query(
+      `SELECT
+        C.id, C.content, C.createdDate, U.id AS userId, U.firstName, U.lastName
+        FROM
+          comments C
+        INNER JOIN
+          users U ON C.userId = U.id
+        WHERE C.id = ? AND C.deletedDate IS NULL;`, [id]);
     return comments[0];
   },
   findCommentsByPostId: async (id: number): Promise<ICommentSQL[]> => {
-    const [comments]: [ICommentSQL[], FieldPacket[]] = await pool.query('SELECT * FROM comments WHERE postId = ? AND deletedDate IS NULL;', [id]);
+    const [comments]: [ICommentSQL[], FieldPacket[]] = await pool.query(
+      `SELECT
+        C.id, C.content, C.createdDate, U.id AS userId, U.firstName, U.lastName
+        FROM
+          comments C
+        INNER JOIN
+          users U ON C.userId = U.id
+        WHERE C.postId = ? AND C.deletedDate IS NULL;`, [id]);
     return comments;
   },
   createComment: async  (newComment: IComment): Promise<number> => {
